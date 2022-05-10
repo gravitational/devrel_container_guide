e5f45f9308d60bbca02e027c1db3e3916668304e
Ben Arent <ben@goteleport.com>
2022-05-10 12:32:56 -0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -16,7 +16,7 @@ def route():
 def download_file():
     print("About to download the file....")
     # to execute wget command on the system and save the output in a file
-    os.system('wget https://sample-videos.com/csv/Sample-Spreadsheet-10-rows.csv -O sample.csv')
+    os.system('wget https://gist.githubusercontent.com/benarent/524f1745e49c0b416e4487bb85063fec/raw/80a537d4d1efdcef31443801aacc891a446f2bdd/SampleCSVFile_119kb.csv -O sample.csv')
     return "Done downloading the file."
 
 if __name__ == '__main__':
